1bf01e17a55052d9531e449a13a2bfbfe05f5c86
Jacky <jacky8595125@gmail.com>
2017-10-03 13:07:22 +0800

    補推

--- ClientApp/routes.js
+++ ClientApp/routes.js
@@ -70,7 +70,6 @@ import v16 from './views/Backend/ReactV16/v16'
 export default (
   <Switch>
     <EmptyRoute path='/' exact component={Template} />
-    <FrontRoute path='/' exact component={Home} />
     <FrontRoute path='/Index' exact component={Home} />
     <LoginRoute path='/login' component={Login} />
     <LoginRoute path='/forgot' component={Forgot} />
